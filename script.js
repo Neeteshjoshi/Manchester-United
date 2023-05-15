f8ef7d7731ae2ef48f0d591d343a501e352ab77c
Nitesh Joshi <neeteshjoshi2021@gmail.com>
2023-05-15 14:56:59 +0545

    added videos and shop

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-let profileBtn = document.getElementById('profileBtn');
-let profileBox = document.getElementById('profileContainer');
-let profileCancel = document.getElementById('profileCancel')
-let container = document.getElementById('mainSection')
-let logoutBox = document.getElementById('logoutBox')
-let logoutBtn = document.getElementById('logoutBtn')
-let logoutCancel = document.getElementById('logoutCancel')
+let profileBtn = document.getElementById("profileBtn");
+let profileBox = document.getElementById("profileContainer");
+let profileCancel = document.getElementById("profileCancel");
+let container = document.getElementById("mainSection");
+let logoutBox = document.getElementById("logoutBox");
+let logoutBtn = document.getElementById("logoutBtn");
+let logoutCancel = document.getElementById("logoutCancel");
 
 function profileClick() {
-    profileBox.style.display = "block";
-    logoutBox.style.display = "none";
+  profileBox.style.display = "block";
+  logoutBox.style.display = "none";
 }
 
 function profileCancelFun() {
-    profileBox.style.display = "none";
+  profileBox.style.display = "none";
 }
 
-function logoutClick () {
-    logoutBox.style.display = "block";
-    profileBox.style.display = "none";
+function logoutClick() {
+  logoutBox.style.display = "block";
+  profileBox.style.display = "none";
 }
 
 function logoutCancelFun() {
-    logoutBox.style.display = "none";
+  logoutBox.style.display = "none";
 }
